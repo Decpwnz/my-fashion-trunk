120dd8d95cc0f1328e8a7512c41f63248eb3f703
Marijus <electromenn@gmail.com>
2024-11-12 18:33:24 +0200

    feat: implement CRUD operations inside items.service, update items.controller to use service

--- backend/src/items/items.controller.ts
+++ backend/src/items/items.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Item> {
+    return this.itemsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() item: Item): Promise<Item> {
+    return this.itemsService.create(item);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<Item> {
+    return this.itemsService.delete(id);
+  }
+}
--- backend/src/items/items.service.ts
+++ backend/src/items/items.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(@InjectModel('Item') private itemModel: Model<Item>) {}
+
+  async findAll(): Promise<Item[]> {
+    return this.itemModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Item> {
+    return this.itemModel.findById(id).exec();
+  }
+
+  async create(item: Item): Promise<Item> {
+    const newItem = new this.itemModel(item);
+    return newItem.save();
+  }
+
+  async delete(id: string): Promise<Item> {
+    return this.itemModel.findByIdAndDelete(id).exec();
+  }
+}
