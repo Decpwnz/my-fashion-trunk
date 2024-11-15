fc8e5732145f35ffec77048b96daf857169dc37e
Marijus <electromenn@gmail.com>
2024-11-15 14:25:30 +0200

    backend test 7

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log('backend test 5');
+    console.log('backend test 6');
     return this.appService.getHello();
   }
 }
