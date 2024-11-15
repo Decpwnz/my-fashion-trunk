4dd342b992f61e0546432a6626c29b6d079d4840
Marijus <electromenn@gmail.com>
2024-11-15 13:23:12 +0200

    deploy test 8

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import styles from './App.module.css'
 
 function App() {
   const { loading } = useAppSelector((state) => state.upload)
-  // deploy test  7
+  // deploy test  8
   return (
     <div className={styles.container}>
       <h1>Image Analysis Tool</h1>
