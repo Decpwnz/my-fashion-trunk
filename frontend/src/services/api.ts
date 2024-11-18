e198a77ae38ffb6235fc386382706a34fde46147
Marijus <electromenn@gmail.com>
2024-11-18 23:04:22 +0200

    configure application load balancer FE

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import type { Category, UploadResponse, NewCategory } from '../types'
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  'my-fashion-trunk-backend-alb-203446907.eu-north-1.elb.amazonaws.com'
 
 export const uploadImage = async (file: File): Promise<UploadResponse> => {
   const formData = new FormData()
