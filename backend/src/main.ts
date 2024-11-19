0f07d9c5c976e9f84ceb8bebb9fe4e59248f759f
Marijus <electromenn@gmail.com>
2024-11-19 12:14:43 +0200

    cors test 3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,22 +5,12 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
-
-  // ec2-13-48-127-146.eu-north-1.compute.amazonaws.com  frontend
-  // ec2-16-171-197-254.eu-north-1.compute.amazonaws.com  backend
-  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
-
+  console.log('hello');
   app.enableCors({
-    origin:
-      // process.env.NODE_ENV === 'production'
-      //   ? 'http://my-fashion-trunk-alb-1332421404.eu-north-1.elb.amazonaws.com/'
-      //   : '*',
-      [
-        'http://my-fashion-trunk-alb-1332421404.eu-north-1.elb.amazonaws.com/',
-        'http://localhost:4173',
-        '*',
-      ],
+    origin: (origin, callback) => {
+      console.log('Incoming request from origin:', origin);
+      callback(null, true);
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
